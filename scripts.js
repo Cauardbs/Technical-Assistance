392584a27a654c98dfeddd4734ae06ecbabf6a03
Cauardbs <122756389+Cauardbs@users.noreply.github.com>
2023-08-26 21:31:19 -0300

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,64 @@
+setTimeout(function () {
+  $('.logo').attr(
+    'src',
+    `
+    https://dewey.tailorbrands.com/production/brand_version_mockup_image/649/8196729649_245fd97a-ae38-403c-8a8e-8bab4f5174d8.png?cb=1678637238
+  `,
+  );
+}, 4500);
+
+setTimeout(function () {
+  $('.logo').attr(
+    'src',
+    `
+      https://dewey.tailorbrands.com/production/brand_version_mockup_image/845/8133149845_06ebd9c9-9064-4e3c-b126-4b810494a0d8.png?cb=1676394766`,
+  );
+}, 5500);
+
+setTimeout(function () {
+  $('.logo').attr(
+    'src',
+    `
+    https://dewey.tailorbrands.com/production/brand_version_mockup_image/842/8196703842_1c1d5188-68c6-4f92-99a6-97e1c2d32047.png?cb=1678636424`,
+  );
+}, 13500);
+
+setTimeout(function () {
+  $('.logo').attr(
+    'src',
+    `
+    https://dewey.tailorbrands.com/production/brand_version_mockup_image/845/8133149845_06ebd9c9-9064-4e3c-b126-4b810494a0d8.png?cb=1676394766`,
+  );
+}, 14500);
+
+setInterval(() => {
+  setTimeout(() => {
+    $('.logo').attr(
+      'src',
+      `
+      https://dewey.tailorbrands.com/production/brand_version_mockup_image/649/8196729649_245fd97a-ae38-403c-8a8e-8bab4f5174d8.png?cb=1678637238`,
+    );
+  }, 4000);
+  setTimeout(() => {
+    $('.logo').attr(
+      'src',
+      `
+      https://dewey.tailorbrands.com/production/brand_version_mockup_image/845/8133149845_06ebd9c9-9064-4e3c-b126-4b810494a0d8.png?cb=1676394766`,
+    );
+  }, 5000);
+
+  setTimeout(() => {
+    $('.logo').attr(
+      'src',
+      `
+      https://dewey.tailorbrands.com/production/brand_version_mockup_image/842/8196703842_1c1d5188-68c6-4f92-99a6-97e1c2d32047.png?cb=1678636424`,
+    );
+  }, 13000);
+  setTimeout(() => {
+    $('.logo').attr(
+      'src',
+      `
+      https://dewey.tailorbrands.com/production/brand_version_mockup_image/845/8133149845_06ebd9c9-9064-4e3c-b126-4b810494a0d8.png?cb=1676394766`,
+    );
+  }, 14000);
+}, 18000);
